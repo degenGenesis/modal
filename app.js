14fa9b54d6e86a5b2de0c3b96ad4245479cbe3fa
Trevor Jones <degengenesis@protonmail.com>
2022-10-01 23:35:52 -0700

    Add: markup & show/hide modal logic

--- app.js
+++ app.js
@@ -3,3 +3,14 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
+const modalBtn = document.querySelector('.modal-btn');
+const overlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', () => {
+  overlay.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+  overlay.classList.remove('open-modal');
+});
